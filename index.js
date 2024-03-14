5aa3d63c62a76bbdbd49f6f4097ca9fe67f5f5b6
chaotic <mnrealest161@gmail.com>
2024-03-14 20:29:23 +0700

    General configuration

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Trang chủ</h1>");
+})
+app.get("/products", (req, res) => {
+    res.send("<h1>Trang sản phẩm</h1>");
+})
+
+app.listen(port, () => {
+    console.log("App is running on " + port);
+})
