4564ae341445d9d3edfc76ef944941b34e16202a
chaotic <mnrealest161@gmail.com>
2024-03-15 17:12:22 +0700

    connect to database

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const routesClient = require("./routes/client/index.route");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
+
+mongoose.connect("mongodb://localhost:27017/product-management")
+    .then(() => console.log("Connected!!!"))
 
 const app = express();
 const port = process.env.PORT;
