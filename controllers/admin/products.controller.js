72c86deedc05d0e06b3ea8fa106c7553894f848e
chaotic <mnrealest161@gmail.com>
2024-03-16 19:27:49 +0700

    get data n make a product management page - admin

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,6 +1,15 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/products.model");
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    })
+
+    console.log(products);
+
     res.render("./admin/pages/products/index.pug", {
-        pageTitle: "Trang Quản Lý Sản Phẩm"
+        pageTitle: "Trang Quản Lý Sản Phẩm",
+        products: products
     });
 }
 
